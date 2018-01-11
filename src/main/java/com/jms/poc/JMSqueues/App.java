60a1aa683e8d54c764c95aac84030b922bf5398c
achasiri <achasiri@gmail.com>
2018-01-11 10:58:52 -0600

    POC on types of JMS

---
+++ src/main/java/com/jms/poc/JMSqueues/App.java
@@ -0,0 +1,13 @@
+package com.jms.poc.JMSqueues;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
